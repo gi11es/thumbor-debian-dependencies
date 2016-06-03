526c9c0192f6800157d45415e3b85ae4970d4950
Gilles Dubuc <gilles@wikimedia.org>
2016-06-03 13:59:25 +0000

    Remove unneeded backports

--- pep8/pep8-1.7.0/debian/tests/smoketest-2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cat > $ADTTMP/E40.py <<EOF
-import os, sys
-EOF
-
-python -m pep8 $ADTTMP/E40.py | grep E401
--- pep8/pep8-1.7.0/debian/tests/smoketest-3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cat > $ADTTMP/E40.py <<EOF
-import os, sys
-EOF
-
-python3 -m pep8 $ADTTMP/E40.py | grep E401
-pep8 $ADTTMP/E40.py | grep E401

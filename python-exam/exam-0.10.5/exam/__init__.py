d0935f024d948384b4edf6f3ba77c3d1b0858f6d
Gilles Dubuc <gilles@wikimedia.org>
2016-06-15 11:57:53 +0000

    Remove packages that have been uploaded to Debian

--- python-exam/exam-0.10.5/exam/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import absolute_import
-
-from exam.cases import Exam  # NOQA
-from exam.helpers import intercept  # NOQA
-from exam.decorators import before, after, around, fixture, patcher  # NOQA
--- python-exam/exam-0.10.5/exam/objects.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-
-
-def always(value):
-    return lambda *a, **k: value
-
-noop = no_op = always(None)

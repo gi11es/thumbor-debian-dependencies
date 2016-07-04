ce64cfd478c19f387df0dd272468b6e86c6c1fd5
Gilles Dubuc <gilles@wikimedia.org>
2016-07-04 16:19:58 +0000

    Remove unneeded dependencies

--- python-pyvows/pyvows-2.0.6/tests/assertions/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# pyvows testing engine
-# https://github.com/heynemann/pyvows
-
-# Licensed under the MIT license:
-# http://www.opensource.org/licenses/mit-license
-# Copyright (c) 2011 Bernardo Heynemann heynemann@gmail.com
